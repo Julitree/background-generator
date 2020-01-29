89517ec4b2acce2c95acc3ec9649adc75318e764
julytree@hotmail.es <Dulcecafe1981>
2020-01-29 10:45:20 +0100

    adding bundle

---
+++ bundle.js
@@ -0,0 +1,32 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+
+  var css = document.getElementsByTagName("h3")[0];
+  var body = document.getElementById("gradient");
+  var color1 = document.querySelector(".color1");
+  var color2 = document.querySelector(".color2");
+ 
+
+
+    var elem = document.getElementById("gradient");
+    var theCSSprop = window.getComputedStyle(elem).getPropertyValue("background");
+    console.log(theCSSprop)
+    document.getElementById("demo").innerHTML = theCSSprop;
+
+
+
+function setgradient(){
+    body.style.background = "linear-gradient(to right," + color1.value + ", " 
+    + color2.value+ ")";
+    css.textContent= body.style.background + ";";
+   
+}
+
+function randomData() {
+    document.getElementById("randomNum").innerHTML = Math.random()
+}
+
+color1.addEventListener("input", setgradient);
+color2.addEventListener("input", setgradient);
+
+
+},{}]},{},[1]);
